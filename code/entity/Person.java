9f3c58050244c27c07599cdf161f5be03a039837
Jasper Pan <105698516+jasperpan8@users.noreply.github.com>
2022-05-26 03:14:03 -0500

    Add files via upload

---
+++ code/entity/Person.java
@@ -0,0 +1,46 @@
+package com.example.restapidemo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
